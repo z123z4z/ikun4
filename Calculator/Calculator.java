0bc897c79198ed42c2f6f7dfeb941475533fdaa7
zhangzz <3010218622@qq.com>
2022-11-14 01:19:54 +0800

    05

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -12,11 +12,12 @@ public class Calculator {
         op1=sc.nextDouble();
         System.out.println("Please input second Double Number:");
         op2=sc.nextDouble();
-        System.out.print("Add Sub Mul Div Resultes:\n");
+        System.out.print("Add Sub Mul Div Mod Resultes:\n");
         Calculate cal=new Calculate();
         cal.doCalculate(new Add(),op1,op2);
 	cal.doCalculate(new Sub(),op1,op2);
         cal.doCalculate(new Mul(),op1,op2);
         cal.doCalculate(new Div(),op1,op2);
+	cal.doCalculate(new Mod(),op1,op2);
     }
 }
